546681b78260515bfb1de375e8922de8a081fb39
Lars Greiss <kufikugel@googlemail.com>
2013-02-14 03:26:27 +0100

    Revert "fix typo for exception message"
    This reverts commit 3757a8ee9f7b09c8aedb49d0a0971e6e63aad698
    
    Change-Id: I6282d97c525d5595a8b492cc088fc109895ca97c

--- src/com/android/providers/telephony/TelephonyProvider.java
+++ src/com/android/providers/telephony/TelephonyProvider.java
@@ -330,13 +330,9 @@ public class TelephonyProvider extends ContentProvider
                         }
                     }
                 } catch (XmlPullParserException e)  {
-                    Log.e(TAG, "Got execption while loading apns.", e);
+                    Log.e(TAG, "Got execption while getting perferred time zone.", e);
                 } catch (IOException e) {
-                    Log.e(TAG, "Got IOExecption while loading apns.", e);
-                } catch (Exception e){
-                    Log.e(TAG, "Exception while loading apns.", e);
-                } finally {
-                    db.endTransaction();
+                    Log.e(TAG, "Got execption while getting perferred time zone.", e);
                 }
             }
         }
